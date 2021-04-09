f1435751c3afa4506a938b786d00534694db8000
Paul Cueva <pcueva.g.com>
2021-04-09 14:56:57 -0500

    validacion-proveedores

--- src/app/pages/home-provider/home-provider.component.ts
+++ src/app/pages/home-provider/home-provider.component.ts
@@ -8,8 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class HomeProviderComponent implements OnInit {
 
   constructor() { }
+  dtOptions:DataTables.Settings={}
 
   ngOnInit(): void {
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 4,
+      language: {
+        url: '//cdn.datatables.net/plug-ins/1.10.24/i18n/Spanish.json'
+      }
+    };
   }
 
 }
--- src/app/pages/register-provider/register-provider.component.ts
+++ src/app/pages/register-provider/register-provider.component.ts
@@ -1,14 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-register-provider',
   templateUrl: './register-provider.component.html',
   styleUrls: ['./register-provider.component.css']
 })
 export class RegisterProviderComponent implements OnInit {
+  
+  datosForm = this.fb.group({
+    email: ['',[Validators.required, Validators.email]],
+    ruc: ['',[Validators.required]],
+    nombreempresa: ['',[Validators.required]],
+    direccion: ['',[Validators.required]],
+    nombrerepresentante: ['',[Validators.required]],
+    apellidos: ['',[Validators.required]],
+    estado: ['',[Validators.required]]
+});
+  constructor(private fb:FormBuilder) { }
 
-  constructor() { }
-
+  onSetDatos(event){
+    event.preventDefault();
+    if(this.datosForm.valid){
+        //this.r.navigate(['/provider']);
+        this.setData(this.datosForm.value.correo);
+        console.log(this.datosForm.value);
+    } else {
+        alert('Faltan ingresar datos o Datos incorrectos');
+    }
+}
+setData(user){
+        sessionStorage.setItem('user',user);
+        let a = document.createElement("a");
+		a.href = "/homeprovider";
+		a.target = "_SELF";
+		a.click();
+    }
   ngOnInit(): void {
   }
 
